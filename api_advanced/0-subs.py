4095beacf83c2ebac6386be23c51f5ede36b5ce1
Max Le <gop_max@m1.com.sg>
2024-03-23 14:52:24 +1100

    remove headers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -16,10 +16,11 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     # Set a custom User-Agent to avoid getting blocked by Reddit's API for too many requests
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    # headers = {'User-Agent': 'Mozilla/5.0'}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        # response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data['data'].get('subscribers', 0)

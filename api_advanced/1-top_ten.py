3c5bf02443cdb18cb42c6b13a64e93a3b22dae11
Max Le <gop_max@m1.com.sg>
2024-03-28 10:16:12 +1100

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import requests
 """
 Write a function that queries the Reddit API
 and prints the titles of the first 10 hot posts listed for a given subreddit
 """
+
+import requests
+
 
 def top_ten(subreddit):
     """

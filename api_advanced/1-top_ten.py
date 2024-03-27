9e7874b70cb2b21e2680b8f16de861bcbe79d348
Max Le <gop_max@m1.com.sg>
2024-03-28 10:14:34 +1100

    modify user agent

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts listed for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyUserAgent'}
+    headers = {'User-Agent': 'Reddit'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:

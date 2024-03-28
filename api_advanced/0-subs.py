ffb52384444e1d8b5b481fe5ba45b10551aac743
Max Le <gop_max@m1.com.sg>
2024-03-28 12:23:36 +1100

    revert back

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
-
 """
-This module queries the Reddit API to return the number of subscribers for a given subreddit.
-If the subreddit is invalid or does not exist, it returns 0.
+0-subs
 """
 
 import requests
 
+# def number_of_subscribers(subreddit):
+#     """
+#     Queries the Reddit API for the number of subscribers to a given subreddit.
+
+#     :param subreddit: The name of the subreddit to query.
+#     :return: The number of subscribers if valid, 0 otherwise.
+#     """
+#     # Define the URL for the subreddit's information using Reddit's API
+#     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+#     # Set a custom User-Agent to avoid getting blocked by Reddit's API for too many requests
+#     headers = {'User-Agent': 'Redditbot'}
+
+#     try:
+#         response = requests.get(url, headers=headers)
+#         if response.status_code == 200:
+#             data = response.json()
+            
+#             return data['data'].get('subscribers', 0)
+#         else:
+#             return 0
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         return 0
 
 def number_of_subscribers(subreddit):
-  '''
-    Returns the number of subscribers for a subreddit.
-
-    Args:
-    subreddit (str): The subreddit name to query.
-
-    Returns:
-    int: The number of subscribers if the subreddit exists, otherwise 0.
-    '''
-  url = f'https://www.reddit.com/r/{subreddit}/about.json'
-  headers = {
-    'User-Agent': 'python:edu.holberton.apiadvanced:v1.0.0 (by /u/MintyGreen15)'
-}
-  s = requests.Session()
-
-  r  = s.get(url, headers=headers, allow_redirects=False)
-  if r.status_code == 200:
-    return r.json()['data']['subscribers']
-  else:
-    return 0
+        """
+        Returns the number of subscribers.
+        """
+        r = requests.get(
+            'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={'User-Agent': 'Redditbot'})
+        r = r.json()
+        try:
+                return(int(r['data']['subscribers']))
+        except:
+                return(0)
